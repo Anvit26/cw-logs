b5a7c4ad3a1a7d7a1c118a1452b715c0e6665789
Anvit26 <57208745+Anvit26@users.noreply.github.com>
2020-02-07 15:08:45 +0530

    Create describeLogStreams.js

---
+++ describeLogStreams.js
@@ -0,0 +1,22 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = "AKIAR4WWKPU4ZUOG5TGO";
+AWS.config.secretAccessKey = "secryArq5wygBYOT1uVQISfGGTaAGyofXqTpVVkQHsr9etKey";
+AWS.config.region = "ap-south-1";
+
+var cloudwatchlogs = new AWS.CloudWatchLogs({apiVersion: '2014-03-28'});
+
+//describeLogStreams
+  var params = {
+        logGroupName: '/aws/lambda/form', // required 
+        descending: true || false,
+        limit: '20',
+        logStreamNamePrefix: '2020/02/03/[$LATEST]f87a1d0cdde041629cd54daff5c4f369',
+        nextToken: null
+    };    
+      cloudwatchlogs.describeLogStreams(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+      });
